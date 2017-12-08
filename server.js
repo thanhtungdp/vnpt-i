ea152aadb1a1ca697f476156727353654868a283
Phan Thanh Tùng <tungptkh@gmail.com>
2017-12-08 14:47:38 +0700

    update for config server

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const config = require('./src/config')
+
+var app = express()
+
+app.use(express.static(path.join(__dirname, 'build')))
+app.use(bodyParser.urlencoded({ extended: true, limit: '100mb' }))
+app.use(bodyParser.json())
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, ''))
+
+app.get('*', function(req, res, next) {
+  res.redirect('/')
+})
+
+app.listen(config.PORT_DEPLOY, () => {
+  console.log('App listen on ' + config.PORT_DEPLOY)
+})
--- src/config.js
+++ src/config.js
@@ -1,9 +1,16 @@
-export const AUTH_API = 'https://59ddd18e4c181d0012a8ce5c.mockapi.io/auth/login'
-export const MENU_API = 'https://59ddd2754c181d0012a8ce60.mockapi.io/menu/menu'
-export const MANAGER_API = 'http://localhost:1234'
+const AUTH_API = 'https://59ddd18e4c181d0012a8ce5c.mockapi.io/auth/login'
+const MENU_API = 'https://59ddd2754c181d0012a8ce60.mockapi.io/menu/menu'
+const MANAGER_API = 'http://localhost:1234'
+const PORT_DEPLOY = 3000;
 
-export default {
-  AUTH_API,
-  MENU_API,
-  MANAGER_API
+module.exports.AUTH_API = AUTH_API
+module.exports.MENU_API = MENU_API
+module.exports.MANAGER_API = MANAGER_API
+module.exports.PORT_DEPLOY = PORT_DEPLOY
+
+module.exports.default = {
+	AUTH_API,
+	MENU_API,
+	MANAGER_API,
+	PORT_DEPLOY
 }
