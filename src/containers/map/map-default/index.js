7ca4aab39151df7c3a16e3711ba55aec2f221a35
MacOS 10.12 <macos10.12@Phan-Tung-Macbook.local>
2017-12-11 13:15:57 +0700

    update map standard

--- src/containers/map/map-default/index.js
+++ src/containers/map/map-default/index.js
@@ -12,9 +12,7 @@ const {
   MarkerWithLabel
 } = require('react-google-maps/lib/components/addons/MarkerWithLabel')
 
-const MapDefaultContainer = styled.div`
-    
-`
+const MapDefaultContainer = styled.div``
 
 @withScriptjs
 @withGoogleMap
@@ -22,31 +20,6 @@ class CustomGoogleMap extends PureComponent {
   render() {
     return (
       <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
-	      <InfoBox
-		      defaultPosition={new google.maps.LatLng(props.center.lat, props.center.lng)}
-		      options={{ closeBoxURL: ``, enableEventPropagation: true }}
-	      >
-		      <div style={{ backgroundColor: `yellow`, opacity: 0.75, padding: `12px` }}>
-			      <div style={{ fontSize: `16px`, fontColor: `#08233B` }}>
-				      Hello, Taipei!
-			      </div>
-		      </div>
-	      </InfoBox>
-	      <Marker
-		      position={{ lat: 22.6273, lng: 120.3014 }}
-		      onClick={props.onToggleOpen}
-	      >
-		      {props.isOpen && <InfoBox
-			      onCloseClick={props.onToggleOpen}
-			      options={{ closeBoxURL: ``, enableEventPropagation: true }}
-		      >
-			      <div style={{ backgroundColor: `yellow`, opacity: 0.75, padding: `12px` }}>
-				      <div style={{ fontSize: `16px`, fontColor: `#08233B` }}>
-					      Hello, Kaohsiung!
-				      </div>
-			      </div>
-		      </InfoBox>}
-	      </Marker>
         {this.props.isMarkerShown &&
           <MarkerWithLabel
             position={{ lat: -34.397, lng: 150.644 }}
