d80d3df2fb338ea63e5ac8547eafbfc7b84b67ef
thanhtungdp <tungptkh@gmail.com>
2017-12-14 09:35:22 +0700

    update config port

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const AUTH_API = 'https://59ddd18e4c181d0012a8ce5c.mockapi.io/auth/login'
 const MENU_API = 'https://59ddd2754c181d0012a8ce60.mockapi.io/menu/menu'
 const MANAGER_API = 'http://localhost:1234'
-const PORT_DEPLOY = 3000;
+const PORT_DEPLOY = 1008;
 
 module.exports.AUTH_API = AUTH_API
 module.exports.MENU_API = MENU_API
