c2a7921bb61a07ec8ef692422e958dfa77b6081d
duong.nguyen@vietan-software.com <duong.nguyen@vietan-software.com>
2018-02-02 13:30:28 +0700

    yarn run fixcode

--- src/containers/avg-search/avg-data-form/index.js
+++ src/containers/avg-search/avg-data-form/index.js
@@ -71,7 +71,10 @@ class AvgDataForm extends React.Component {
       var line = (
         <LineSeries
           id={dataLines[item].key}
-          name={dataLines[item].name + (dataLines[item].unit ? '(' + dataLines[item].unit + ')' : '')}
+          name={
+            dataLines[item].name +
+            (dataLines[item].unit ? '(' + dataLines[item].unit + ')' : '')
+          }
           data={dataLines[item].data}
         />
       )
