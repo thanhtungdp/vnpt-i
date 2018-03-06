9f5aaed0336caf1dd8da98eee6b6e74815c8672d
sevenlure <01226905092>
2018-03-06 16:39:22 +0700

    bo sung icon: large, normal, small

--- src/containers/manager/station-type/station-type-form/index.js
+++ src/containers/manager/station-type/station-type-form/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { Form, Input, Button, Row, Col, Checkbox, Upload, Icon } from 'antd'
+import { Form, Input, Button, Row, Col, Checkbox, Upload, Icon, Modal } from 'antd'
 import PropTypes from 'prop-types'
 import { autobind } from 'core-decorators'
 import { mapPropsToFields } from 'utils/form'
 import { STATION_TYPE_API } from 'config'
-import createLanguageHoc, { langPropTypes } from '../../../../hoc/create-lang'
+import createLanguageHoc, { langPropTypes } from 'hoc/create-lang'
+import swal from 'sweetalert2'
 
 const FormItem = Form.Item
 
@@ -21,6 +22,17 @@ export default class StationTypeForm extends React.PureComponent {
     lang: langPropTypes
   }
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      previewVisible: false,
+      previewImage: '',
+      largeImgList: [],
+      normalImgList: [],
+      smallImgList: []
+    }
+  }
+
   handleSubmit(e) {
     e.preventDefault()
     this.props.form.validateFields((err, values) => {
@@ -29,29 +41,100 @@ export default class StationTypeForm extends React.PureComponent {
         key: values.key,
         name: values.name,
         isAuto: values.isAuto,
-        icon: values.icon,
-        files: {}
+        files: {},
+        icon: {
+          large: this.state.largeImgList.length > 0 ? this.state.largeImgList[0].response : null,
+          normal: this.state.normalImgList.length > 0 ? this.state.normalImgList[0].response : null,
+          small: this.state.smallImgList.length > 0 ? this.state.smallImgList[0].response : null
+        }
       }
-      data.icon =
-        values.file && values.file.length > 0
-          ? values.file[0].response.file
-          : values.icon
       // Callback submit form Container Component
       this.props.onSubmit(data)
     })
   }
 
-  normFile(e) {
-    console.log('Upload event:', e)
-    if (Array.isArray(e)) {
-      return e
+  handlePreview = (file) => {
+    this.setState({
+      previewImage: file.url || file.thumbUrl,
+      previewVisible: true,
+    });
+  }
+
+
+  handleCancel = () => {
+    this.setState({ previewVisible: false })
+  }
+
+  handleImageChange(type, fileList, file, event) {
+    for (var i = 0; i < fileList.length; i++) {
+      if (fileList[i].thumbUrl != '') {
+        fileList[i].status = 'done'
+      }
     }
-    return e && e.fileList
+
+    //error
+    if (file.status == 'error') {
+      fileList = []
+      swal({
+        title: 'upload image fail',
+        type: 'error'
+      })
+    }
+
+    this.setState({ [type + 'ImgList']: fileList })
+  }
+
+  renderButtonUpload(name) {
+    return <div>
+      <Icon type="plus" />
+      <div className="ant-upload-text">{name}</div>
+    </div>
+  }
+
+  async componentWillMount() {
+    if (this.props.initialValues == null)
+      return
+    if (this.props.initialValues.icon == null)
+      return
+
+    //Set image Icon
+    let largeImgList = []
+    let normalImgList = []
+    let smallImgList = []
+    if (this.props.initialValues.icon.large != null && this.props.initialValues.icon.large.file)
+      largeImgList.push({
+        uid: -1,
+        url: this.props.initialValues.icon.large.url,
+        name: this.props.initialValues.icon.large.file.originalname,
+        status: 'done',
+      })
+    if (this.props.initialValues.icon.normal != null && this.props.initialValues.icon.normal.file)
+      normalImgList.push({
+        uid: -1,
+        url: this.props.initialValues.icon.normal.url,
+        name: this.props.initialValues.icon.normal.file.originalname,
+        status: 'done',
+      })
+    if (this.props.initialValues.icon.small != null && this.props.initialValues.icon.small.file)
+      smallImgList.push({
+        uid: -1,
+        url: this.props.initialValues.icon.small.url,
+        name: this.props.initialValues.icon.small.file.originalname,
+        status: 'done',
+      })
+
+    this.setState({
+      largeImgList: largeImgList,
+      normalImgList: normalImgList,
+      smallImgList: smallImgList
+    })
+
   }
 
   render() {
     const { getFieldDecorator } = this.props.form
     const { t } = this.props.lang
+    const { previewVisible, previewImage, fileList } = this.state;
     return (
       <Form onSubmit={this.handleSubmit}>
         <Row gutter={16}>
@@ -66,7 +149,7 @@ export default class StationTypeForm extends React.PureComponent {
                 ]
               })(
                 <Input placeholder={t('stationTypeManager.form.key.label')} />
-              )}
+                )}
             </FormItem>
           </Col>
           <Col span={12}>
@@ -80,29 +163,61 @@ export default class StationTypeForm extends React.PureComponent {
                 ]
               })(
                 <Input placeholder={t('stationTypeManager.form.name.label')} />
-              )}
+                )}
             </FormItem>
           </Col>
         </Row>
         <Row gutter={16}>
           <Col span={12}>
             <FormItem label={t('stationTypeManager.form.icon.label')}>
-              <div className="dropbox">
-                {getFieldDecorator('file', {
-                  valuePropName: 'file',
-                  getValueFromEvent: this.normFile
-                })(
+              <Col span={6}>
+                <div className="dropbox">
                   <Upload
-                    action={urlUpdate}
-                    listType={'picture'}
-                    defaultFileList={[]}
+                    action='http://localhost:1234/photo/uploadWithDirectory/station-types'
+                    listType="picture-card"
+                    fileList={this.state.largeImgList}
+                    onPreview={this.handlePreview}
+                    onChange={({ fileList, file, event }) => {
+                      this.handleImageChange('large', fileList, file, event)
+                    }}
                   >
-                    <Button>
-                      <Icon type="upload" /> upload
-                    </Button>
+                    {this.state.largeImgList.length >= 1 ? null : this.renderButtonUpload('Large')}
                   </Upload>
-                )}
-              </div>
+                  <Modal visible={previewVisible} footer={null} onCancel={this.handleCancel}>
+                    <img alt="example" style={{ width: '100%' }} src={previewImage} />
+                  </Modal>
+                </div>
+              </Col>
+              <Col span={6}>
+                <div className="dropbox">
+                  <Upload
+                    action='http://localhost:1234/photo/uploadWithDirectory/station-types'
+                    listType="picture-card"
+                    fileList={this.state.normalImgList}
+                    onPreview={this.handlePreview}
+                    onChange={({ fileList, file, event }) => {
+                      this.handleImageChange('normal', fileList, file, event)
+                    }}
+                  >
+                    {this.state.normalImgList.length >= 1 ? null : this.renderButtonUpload('Normal')}
+                  </Upload>
+                </div>
+              </Col>
+              <Col span={6}>
+                <div className="dropbox">
+                  <Upload
+                    action='http://localhost:1234/photo/uploadWithDirectory/station-types'
+                    listType="picture-card"
+                    fileList={this.state.smallImgList}
+                    onPreview={this.handlePreview}
+                    onChange={({ fileList, file, event }) => {
+                      this.handleImageChange('small', fileList, file, event)
+                    }}
+                  >
+                    {this.state.smallImgList.length >= 1 ? null : this.renderButtonUpload('Small')}
+                  </Upload>
+                </div>
+              </Col>
             </FormItem>
           </Col>
           <Col span={12}>
@@ -111,7 +226,6 @@ export default class StationTypeForm extends React.PureComponent {
             </FormItem>
           </Col>
         </Row>
-        {getFieldDecorator('icon')(<Input type={'hidden'} />)}
         <FormItem>
           <Button style={{ width: '100%' }} type="primary" htmlType="submit">
             Save
