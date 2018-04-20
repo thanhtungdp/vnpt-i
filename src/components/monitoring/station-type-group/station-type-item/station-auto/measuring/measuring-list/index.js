8bb0bffc06805890dfa174c978d263b9630744bf
duong.nguyen@vietan-software.com <duong.nguyen@vietan-software.com>
2018-04-20 13:32:30 +0700

    change size measuring in monitoring

--- src/components/monitoring/station-type-group/station-type-item/station-auto/measuring/measuring-list/index.js
+++ src/components/monitoring/station-type-group/station-type-item/station-auto/measuring/measuring-list/index.js
@@ -15,7 +15,7 @@ const MeasuringListWrapper = styled.div`
 
 const MeasuringItemWrapper = styled.div`
   padding: 0px 8px;
-  width: ${props => (props.navigationIsOpen ? '20%' : '14%')};
+  width: ${props => (props.navigationIsOpen ? '20%' : '12.5%')};
 `
 
 @connect(state => ({
