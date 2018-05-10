403ac51d517c022396a8cccef3549ac1b468e46c
duong.nguyen@vietan-software.com <duong.nguyen@vietan-software.com>
2018-05-10 15:48:07 +0700

    complete subscription

--- src/containers/subscription/subscription-overview/SubscriptionTableList.js
+++ src/containers/subscription/subscription-overview/SubscriptionTableList.js
@@ -9,64 +9,64 @@ const SubscriptionTableListWrapper = styled.div``
 
 @autobind
 export default class SubscriptionTableList extends React.PureComponent {
-  static propTypes = {
-    data: PropTypes.arrayOf(
-      PropTypes.shape({
-        subscriptionId: PropTypes.any,
-        renewAt: PropTypes.any,
-        expiredAt: PropTypes.any
-      })
-    )
-  }
+	static propTypes = {
+		data: PropTypes.arrayOf(
+			PropTypes.shape({
+				subscriptionId: PropTypes.any,
+				renewAt: PropTypes.any,
+				expiredAt: PropTypes.any
+			})
+		)
+	}
 
-  getColumns() {
-    return [
-      {
-        title: '#',
-        dataIndex: 'Index',
-        key: 'index',
-        render(value, record, index) {
-          return <span>{index + 1}</span>
-        }
-      },
-      {
-        title: 'ID',
-        dataIndex: 'subscriptionId',
-        key: 'subscriptionId',
-        render(value, record) {
-          return value
-        }
-      },
-      {
-        title: translate('supscriptionStatus.renewAt'),
-        dataIndex: 'renewAt',
-        key: 'renewAt',
-        render(value) {
-          return value
-        }
-      },
-      {
-        title: translate('supscriptionStatus.expiredAt'),
-        dataIndex: 'expiredAt',
-        key: 'expiredAt',
-        render(value) {
-          return value
-        }
-      }
-    ]
-  }
+	getColumns() {
+		return [
+			{
+				title: '#',
+				dataIndex: 'Index',
+				key: 'index',
+				render(value, record, index) {
+					return <span>{index + 1}</span>
+				}
+			},
+			{
+				title: 'ID',
+				dataIndex: 'subscriptionId',
+				key: 'subscriptionId',
+				render(value, record) {
+					return value
+				}
+			},
+			{
+				title: translate('subscriptionStatus.renewAt'),
+				dataIndex: 'renewAt',
+				key: 'renewAt',
+				render(value) {
+					return value
+				}
+			},
+			{
+				title: translate('subscriptionStatus.expiredAt'),
+				dataIndex: 'expiredAt',
+				key: 'expiredAt',
+				render(value) {
+					return value
+				}
+			}
+		]
+	}
 
-  render() {
-    return (
+	render() {
+		return (
       <SubscriptionTableListWrapper>
         <Table
           size="small"
           rowKey="_id"
           columns={this.getColumns()}
-          {...this.props}
+					{...this.props}
           pagination={false}
         />
       </SubscriptionTableListWrapper>
-    )
-  }
+		)
+	}
 }
