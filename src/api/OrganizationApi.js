c227832604bccfe448d41f16d3622f5a98cca591
duong.nguyen@vietan-software.com <duong.nguyen@vietan-software.com>
2018-05-10 16:46:29 +0700

    fixcode

---
+++ src/api/OrganizationApi.js
@@ -0,0 +1,11 @@
+import { ORGANIZATION_API } from '../config'
+import { getFetch } from 'utils/fetch'
+
+export function getSubscription() {
+	let urlFetch = ORGANIZATION_API + '/organization/subscription/status'
+	return getFetch(urlFetch)
+}
+
+export default {
+	getSubscription
+}
